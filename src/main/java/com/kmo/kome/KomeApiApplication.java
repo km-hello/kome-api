c71fd204cc61a75b87f2b043a572437e9f9c4d9e
kongmeng <km@km-o.com>
2025-11-27 00:12:23 +0800

    Initial commit

---
+++ src/main/java/com/kmo/kome/KomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.kmo.kome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmo/kome/KomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmo.kome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

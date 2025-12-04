2589d508ab8da5287127343cb6ebdb1887bbc64c
kongmeng <km@km-o.com>
2025-12-04 21:48:53 +0800

    feat: add `Post` entity and update `User` entity
    - Introduced `Post` entity to represent blog posts with properties such as title, content, views, and status.
    - Updated `User` entity by changing `isDeleted` field type from `Integer` to `Boolean` for type consistency.
    
    ---
    
    - 新增 `Post` 实体类，用于表示博客文章，包含标题、内容、浏览数和状态等属性。
    - 更新 `User` 实体，将 `isDeleted` 字段类型从 `Integer` 改为 `Boolean`，以实现类型一致性。

---
+++ src/main/java/com/kmo/kome/entity/Post.java
@@ -0,0 +1,38 @@
+package com.kmo.kome.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 博客文章实体类
+ * 对应数据库表: post
+ * 用于存储和管理博客文章的相关信息
+ */
+@Data
+@TableName("post")
+public class Post {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String title;
+    private String summary;
+    private String content;
+    private String coverImage;
+    private Integer views;
+    private Integer readTime;
+
+    // 状态: 1=已发布, 0=草稿, 默认0
+    private Integer status;
+    // 是否置顶: 1=是, 0=否, 默认0
+    private Boolean isPined;
+    // 逻辑删除
+    @TableLogic
+    private Boolean isDeleted;
+
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
